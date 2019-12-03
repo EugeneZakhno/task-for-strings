1a696f03edd925f9324002841e509fef8aafcc44
EugeneZakhno <zohan1234567@gmail.com>
2019-12-03 21:27:00 +0200

    first commit for String task

---
+++ task01/src/main/java/StringThreeTimes.java
@@ -0,0 +1,6 @@
+public class StringThreeTimes {
+    public static void main(String[] args) {
+        String str = " Hello I'm String, show me three times, and show amount of symbols";
+        System.out.println(str + "\n" + str + "\n" + str + "\n" +  " " + str.length());
+    }
+}
