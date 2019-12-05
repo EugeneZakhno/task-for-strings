650b25eec65908a9da98c098e2de06c0252d2fda
EugeneZakhno <zohan1234567@gmail.com>
2019-12-05 10:14:19 +0200

    commit for task-4

--- task04/src/main/java/OddEvenLetters.java
+++ task04/src/main/java/OddEvenLetters.java
@@ -6,12 +6,13 @@ public class OddEvenLetters {
 
         String str = "skvskvbvdfblo";
 
-        for (int i = 0; i < 10; i += 2) {
-            System.out.print(i+"");
-            for (int j = 1; j < str.length(); j += 2) {
-                System.out.print(str.charAt(j));
-            }
-        }
+        for (int i = 2; i < 9; i += 2) {
 
+             for (int j = 1; j < i-1; j += 2) {
+                 System.out.print(i+"");
+                 System.out.print(str.charAt(j));
+                }
+
+        }
     }
 }
