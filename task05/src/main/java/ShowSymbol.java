05e51fcffde0fb08562616d92f8c847bed8a4f22
EugeneZakhno <zohan1234567@gmail.com>
2019-12-06 21:20:21 +0200

    refactoring one more time task04

--- task05/src/main/java/ShowSymbol.java
+++ task05/src/main/java/ShowSymbol.java
@@ -1,16 +1,13 @@
 public class ShowSymbol {
     public static void main(String[] args) {
 
-       // 5. Дана строка. Показать номера символов, совпадающих с последним символом строки.
+        // 5. Дана строка. Показать номера символов, совпадающих с последним символом строки.
 
-        String str = "hello d myd friend";
+        String str = "heldlo d myd friend";
 
-        System.out.println(str.indexOf('o'));
-        for (int i=0; i<str.length();i++) {
-            System.out.println(str.indexOf("d"));
-            if (str.length() - 1 == str.charAt(i)) {
 
-            }
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.indexOf('d'));
         }
     }
 }
