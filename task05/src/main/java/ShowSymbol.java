2c89ab07af75d9f74edef3aea59ede53e85a0103
EugeneZakhno <zohan1234567@gmail.com>
2019-12-08 22:00:59 +0200

    fixed task05

--- task05/src/main/java/ShowSymbol.java
+++ task05/src/main/java/ShowSymbol.java
@@ -6,9 +6,8 @@ public class ShowSymbol {
         String str = "hdeldlo d myd friend";
 
         for (int i = 0; i < str.length(); i++) {
-                if(str.charAt(str.length()-1)== str.charAt(i))
-                  //  System.out.println(str.indexOf(str.charAt(i)));
-                    System.out.print(str.indexOf(i));
+                if(str.charAt(str.length()-1) == str.charAt(i))
+                    System.out.print(str.indexOf(str.charAt(i)));
             }
         }
     }
